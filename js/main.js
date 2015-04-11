7db3e1efe4b65a502cb5d7bfca27bc22e803d79f
prashanth9962 <prashanth.engr@gmail.com>
2015-04-11 18:42:38 +0530

    fix for header height for smaller resolution

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
   //xPosition - Horizontal position of the element
   //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
   //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
-  $('#top').parallax("50%", 0.4);
+ // $('#top').parallax("50%", 0.4);
   $('#testimonial').parallax("50%", 0.4);
   $('#download').parallax("50%", 0.4);
 })
